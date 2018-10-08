102797dfaafa3104d4d2e36447eb763207ee5059
Jane <jane@Janes-MacBook-Pro.local>
2018-10-08 12:06:48 -0700

    fix:bug

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -14,25 +14,31 @@ class SearchBooks extends React.Component {
 
   updateQuery(query) {
     this.setState({query: query});
-    BooksAPI.search(query)
-    .then(response => {
-      this.setState({searchBookResults: response, listBooks: []});
-      if(this.state.searchBookResults !== undefined && this.state.searchBookResults.error == null) {
-        this.state.searchBookResults.map((book) => {
-          BooksAPI.get(book.id).then(response => this.setState({listBooks: [...this.state.listBooks, response]}))
-        })
-        this.state.listBooks.sort(sortBy('title'));
-      }
-    })
+    if(query) {
+      BooksAPI.search(query)
+      .then(response => {
+        this.setState({searchBookResults: response});
+        if(this.state.searchBookResults !== undefined) {
+          if(this.state.searchBookResults.length>0) {
+            let listBooks = [];
+            this.state.searchBookResults.map((book) => BooksAPI.get(book.id).then(response => listBooks.push(response)));
+            this.setState({listBooks: listBooks});
+          }
+        }
+      })
+    }
   }
 
   renderSearchResults() {
-    if(this.state.searchBookResults === undefined || this.state.searchBookResults.error != null) {
-      return <div>No Matching Results</div>
-    } else {
-      return this.state.listBooks.map((book) => (
-        <Book key={book.id} book={book} onUpdateBook={this.props.onUpdateBook} />
-      ))
+    if(this.state.query !== undefined) {
+      if(this.state.searchBookResults.error != null) {
+        return <div>No Matching Results</div>
+      } else if (this.state.query !== ''){
+        this.state.listBooks.sort(sortBy('title'));
+        return this.state.listBooks.map((book) => (
+          <Book key={book.id} book={book} onUpdateBook={this.props.onUpdateBook} />
+        ))
+      }
     }
   }
 
